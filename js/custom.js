662d1fda19567bd378b10c8db1b3729b9cbdadf2
agusnat <agustin.nat@gmail.com>
2023-03-09 19:54:58 -0300

    minor fixes

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,5 @@ var form = document.querySelector('.pageclip-form')
 Pageclip.form(form, {
   onSubmit: function (event) { },
   onResponse: function (error, response) { },
-  successTemplate: '<div class="ty"><p class="font_1">Gracias por formar parte de esto!</p></div>'
+  successTemplate: '<div class="ty"><p class="font_8">Gracias por ser parte!</p></div>'
 })
