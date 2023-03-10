d0c3f5011672fd1409bd6d22723d950218435fc3
agusnat <agustin.nat@gmail.com>
2023-03-10 01:40:30 -0300

    og tags added, minor css fixes

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,5 @@ var form = document.querySelector('.pageclip-form')
 Pageclip.form(form, {
   onSubmit: function (event) { },
   onResponse: function (error, response) { },
-  successTemplate: '<div class="ty"><p class="font_8">Gracias por ser parte!</p></div>'
+  successTemplate: '<div class="ty"><p class="font_8">Bienvenida/o!</p></div>'
 })
